1d8bf247c4cc08c3c4f0ae7ec232fe1b73e3f0c6
hkenawi <hkenawi7@gmail.com>
2024-04-06 02:17:09 -0500

    Creating consolidation function for player match-log data + adding consolidated file to data directory

--- src/consolidate_data.py
+++ src/consolidate_data.py
@@ -28,13 +28,12 @@ def consolidateFBRefPlayerMatchData() -> pd.DataFrame:
     :return: A stacked dataframe with data for all years
     """
     df = pd.DataFrame()
-    seasons = ['2018_19', '2019_20', '2020_21', '2021_22', '2022_23']
+    seasons = ['2018_2019', '2019_2020', '2020_2021', '2021_2022', '2022_2023']
 
     for season in seasons:
-        temp_df = pd.read_csv(filepath_or_buffer=f'.data/raw data/{season}_player_match_log_data.csv',
+        temp_df = pd.read_csv(filepath_or_buffer=f'./data/raw data/{season}_player_match_log_data.csv',
                               header=1)
         temp_df['season'] = season
-        temp_df = temp_df[temp_df['Rk'].apply(lambda x: str(x).isdigit())]
         df = pd.concat([df, temp_df])
         df.reset_index(drop=True,
                        inplace=True)
@@ -44,7 +43,7 @@ def consolidateFBRefPlayerMatchData() -> pd.DataFrame:
 
 if __name__ == "__main__":
     stacked_offensive_player_df = consolidateFBRefPlayerOffensiveData()
-    stacked_offensive_player_df.to_csv('consolidated_player_offensive_stats.csv')
+    stacked_offensive_player_df.to_csv('./data/consolidated_player_offensive_stats.csv')
 
     stacked_match_log_df = consolidateFBRefPlayerMatchData()
-    stacked_match_log_df.to_csv('consolidated_player_match_log_data.csv')
+    stacked_match_log_df.to_csv('./data/consolidated_player_match_log_data.csv')
